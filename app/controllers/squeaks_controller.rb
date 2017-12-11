511735d2d6d0dc1ba146b926d96d88ae45b248eb
Henry Alpert <halpert3@gmail.com>
2017-12-11 15:29:25 +0000

    Add ability to create squeaks. Setup squeak show view.

--- app/controllers/squeaks_controller.rb
+++ app/controllers/squeaks_controller.rb
@@ -1,13 +1,35 @@
 class SqueaksController < ApplicationController
+  before_action :authenticate_user!
+  
   def index
   end
 
-  def new
+  def new #just puts up the form
+    @squeak = Squeak.new
   end
 
+  def create  
+    @squeak = Squeak.new(squeak_params)
+    
+    if @squeak.save
+      redirect_to @squeak, notice: "You squeaked!"
+    else
+      render :new #if squeak doesn't save, let's them try again. Goes back to 'new' method
+    end
+    
+  end
+  
   def edit
   end
 
   def show
+    @squeak = Squeak.find(params[:id])
   end
+  
+  private
+  
+  def squeak_params
+    params.require(:squeak).permit(:message, :user_id)
+  end
+
 end
